2352020f1fbe138d5be53e9fc051c91d9625b05a
Nicolas191929 <krezu191996@gmail.com>
2020-01-23 10:49:21 +0200

    first commit

---
+++ Project2/Source.cpp
@@ -0,0 +1,27 @@
+﻿#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "iostream"
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src1;
+	src1 = imread("car.jpg", IMREAD_COLOR);
+	namedWindow("Оригінал", WINDOW_AUTOSIZE);
+	imshow("Оригінал", src1);
+
+	Mat gray, edge, draw;
+	cvtColor(src1, gray, COLOR_BGR2GRAY);
+
+	Canny(gray, edge, 50, 150, 3);
+
+	edge.convertTo(draw, CV_8U);
+	namedWindow("Результат", WINDOW_AUTOSIZE);
+	imshow("Результат", draw);
+
+	waitKey(0);
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# opencv_konturi
+Лабораторна робота OpenCV - контури зображення
+
+Оригінал:
+
+![1](https://user-images.githubusercontent.com/8949277/72506826-59e36980-384b-11ea-8a17-8e76ecc21b3b.png)
+
+Результат:
+
+![2](https://user-images.githubusercontent.com/8949277/72506836-5f40b400-384b-11ea-9370-4199d7474183.png)
